f06b26b3ef057571b60f7384bc90eb75f4c3057a
Dong Liu <Luid@INNOPARK.ad>
2019-11-28 18:19:13 +1100

    add pagination support

--- src/data/ActionCreators.js
+++ src/data/ActionCreators.js
@@ -1,10 +1,18 @@
 import { ActionTypes } from "./Types";
-import { data as phData } from "./placeholderData";
+//import { data as phData } from "./placeholderData";
+import { RestDataSource } from "./RestDataSource";
 
-export const loadData = (dataType) => ({
-    type: ActionTypes.DATA_LOAD,
-    payload: {
-        dataType: dataType,
-        data: phData[dataType]
-    }
-});
+const dataSource = new RestDataSource();
+
+export const loadData = (dataType, params) => (
+    {
+        type: ActionTypes.DATA_LOAD,
+        payload: dataSource.GetData(dataType, params).then(response =>
+            ({
+                dataType,
+                data: response.data,
+                total: Number(response.headers["x-total-count"]),
+                params
+            })
+        )
+    })
